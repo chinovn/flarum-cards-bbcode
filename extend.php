b933cc01990faf0f981ef4ed88c5e36d021dcb84
chinovn <110653930+chinovn@users.noreply.github.com>
2022-08-21 13:54:02 +0700

    Update extend.php

--- extend.php
+++ extend.php
@@ -11,14 +11,13 @@ return [
          
         $config->BBCodes->addCustom('[CARD={URL} p={SIMPLETEXT}]{ANYTHING}[/CARD]', '<div class="cb-card"><img src="{URL}"><div class="cb-card-content {SIMPLETEXT}">{ANYTHING}</div></div>');
         
-        $config->BBCodes->addCustom('[CARD1={URL}]{ANYTHING}[/CARD1]', '<div class="cb-card1" style="background-image: url({URL});"><div class="cb-card1-content">{ANYTHING}</div></div>');
+        $config->BBCodes->addCustom('[CARD1={URL}]{ANYTHING}[/CARD1]', '<div class="cb-card1" style="background-image: url({URL}) !important;"><div class="cb-card1-content">{ANYTHING}</div></div>');
         
-        $config->BBCodes->addCustom('[CARD2={URL}]{ANYTHING}[/CARD2]', '<div class="cb-card2"><div class="cb-card2-img" style="background-image: url({URL});"></div><div class="cb-card2-content">{ANYTHING}</div></div>');
+        $config->BBCodes->addCustom('[CARD2={URL}]{ANYTHING}[/CARD2]', '<div class="cb-card2"><div class="cb-card2-img" style="background-image: url({URL}) !important;"></div><div class="cb-card2-content">{ANYTHING}</div></div>');
         
         $config->BBCodes->addCustom('[CARD3={URL}]{ANYTHING}[/CARD3]', '<div class="cb-card3"><div class="cb-card3-img"><img src="{URL}"></div><div class="cb-card3-content">{ANYTHING}</div></div>');
         
-        $config->BBCodes->addCustom('[CARD4={URL} c={COLOR}]{ANYTHING}[/CARD4]', '<div class="cb-card4" style="background: {COLOR}"><img src="{URL}" class="cb-card4-img"><div class="cb-card4-content">{ANYTHING}</div></div>');
-        $config->BBCodes->addCustom('[CARD5={URL} c={COLOR}]{ANYTHING}[/CARD5]', '<div class="cb-card5" style="background: {COLOR}"><img src="{URL}" class="cb-card5-img"><div class="cb-card5-content">{ANYTHING}</div></div>');
-        
+         $config->BBCodes->addCustom('[CARD4={URL} c={COLOR}]{ANYTHING}[/CARD4]', '<div class="cb-card4" style="background: {COLOR} !important;"><img src="{URL}" class="cb-card4-img"><div class="cb-card4-content">{ANYTHING}</div></div>');
+        $config->BBCodes->addCustom('[CARD5={URL} c={COLOR}]{ANYTHING}[/CARD5]', '<div class="cb-card5" style="background: {COLOR} !important;"><img src="{URL}" class="cb-card5-img"><div class="cb-card5-content">{ANYTHING}</div></div>');
     })
 ];
